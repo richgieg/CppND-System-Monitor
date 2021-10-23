3628507869f78f340b48858bd793463a9428f09a
Richard Gieg <richgieg@noreply.users.github.com>
2021-10-22 22:49:33 -0700

    Use 2 spaces for tabs in playground.cpp

--- playground/playground.cpp
+++ playground/playground.cpp
@@ -7,28 +7,28 @@ std::string GetKernel();
 std::string GetKernel2();
 
 int main() {
-    std::string kernel_version = GetKernel2();
-    std::cout << kernel_version << "\n";
+  std::string kernel_version = GetKernel2();
+  std::cout << kernel_version << "\n";
 }
 
 std::string GetKernel() {
-    std::string os, kernel, version;
-    std::string line;
-    std::ifstream stream("/proc/version");
-    if (stream.is_open()) {
-        std::getline(stream, line);
-        std::istringstream linestream(line);
-        linestream >> os >> version >> kernel;
-    }
-    return kernel;
+  std::string os, kernel, version;
+  std::string line;
+  std::ifstream stream("/proc/version");
+  if (stream.is_open()) {
+    std::getline(stream, line);
+    std::istringstream linestream(line);
+    linestream >> os >> version >> kernel;
+  }
+  return kernel;
 }
 
 std::string GetKernel2() {
-    std::string os, kernel, version;
-    std::string line;
-    std::ifstream stream("/proc/version");
-    if (stream.is_open()) {
-        stream >> os >> version >> kernel;
-    }
-    return kernel;
+  std::string os, kernel, version;
+  std::string line;
+  std::ifstream stream("/proc/version");
+  if (stream.is_open()) {
+    stream >> os >> version >> kernel;
+  }
+  return kernel;
 }
