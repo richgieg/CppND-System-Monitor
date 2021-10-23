40374794207a9d95bbdebc4310d9c5f0c5a8e2ce
Richard Gieg <richgieg@noreply.users.github.com>
2021-10-23 10:57:11 -0700

    Complete LinuxParser::UpTime

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -70,7 +70,14 @@ vector<int> LinuxParser::Pids() {
 float LinuxParser::MemoryUtilization() { return 0.0; }
 
 // TODO: Read and return the system uptime
-long LinuxParser::UpTime() { return 0; }
+long LinuxParser::UpTime() {
+  long uptimeSeconds = 0;
+  std::ifstream stream(kProcDirectory + kUptimeFilename);
+  if (stream.is_open()) {
+    stream >> uptimeSeconds;
+  }
+  return uptimeSeconds;
+}
 
 // TODO: Read and return the number of jiffies for the system
 long LinuxParser::Jiffies() { return 0; }
